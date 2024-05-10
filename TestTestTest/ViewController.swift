dbb9114e7e0ae194d9c1a8af15eb0deb30ba2505
Israa <isramuhammed613@gmail.com>
2024-05-10 01:23:50 -0700

    addnav

--- TestTestTest/ViewController.swift
+++ TestTestTest/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nav(_ sender: Any) {
+        
+        
+let sec = self.storyboard?.instantiateViewController(withIdentifier: "sec") as! secViewController
+navigationController?.pushViewController(sec, animated: true)
+    }
+    
 }
 
